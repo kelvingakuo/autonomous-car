5622747261a27b7d77038c3f8f8bb6f1da0e98a6
kelvingakuo <kelvingakuo@gmail.com>
2019-07-23 11:17:51 +0300

    Implemented capturing as a generator instead

--- self_driving.py
+++ self_driving.py
@@ -7,7 +7,6 @@ import socket
 import struct
 import sys
 import time
-import multiprocessing
 from subprocess import call
 
 from picamera import PiCamera
@@ -20,33 +19,37 @@ logger = central_log_config.central_logger
 
 
 # God mode and self_driving in separate processes.
-# Shared variable to indicate when control has been handed back
 
-def godMode():
-	# Double tap 'X' to shut down everything
-	pressedOnce = False 
-	firstTimePress = 0
+# def godMode(cam):
+# 	# Double tap 'X' to shut down everything
+# 	pressedOnce = False 
+# 	firstTimePress = 0
 
-	while True:
-		# 1. 'god mode'
-	 	for event in pygame.event.get():
-	 		if(event.type == pygame.JOYBUTTONDOWN):
-	 			if(event.button == 0): # Shutdown everything
-						if(pressedOnce): # Second press?
-							elapsedTime = time.time() - firstTimePress
-							if(elapsedTime <= 2):
-								cam.close()
-								logger.error("WAS DRIVING, THE DUDE SHUT US DOWN. WE GIF UP!!!")
-								call("sudo killall python", shell = True)
-								sys.exit()
-							else:
-								pressedOnce = False
-								firstTimePress = 0
-						else:
-							pressedOnce = True
-							firstTimePress = time.time()
+# 	pygame.init()
+# 	stick = pygame.joystick.Joystick(0)
+# 	stick = stick.init()
 
-def getPreds(outputQueue, cam):
+# 	while True:
+# 		# 1. 'god mode'
+# 	 	for event in pygame.event.get():
+# 	 		if(event.type == pygame.JOYBUTTONDOWN):
+# 	 			if(event.button == 0): # Shutdown everything
+# 						if(pressedOnce): # Second press?
+# 							elapsedTime = time.time() - firstTimePress
+# 							if(elapsedTime <= 2):
+# 								cam.close()
+# 								logger.error("WAS DRIVING, THE DUDE SHUT US DOWN. WE GIF UP!!!")
+# 								call("sudo killall python", shell = True)
+# 								sys.exit()
+# 							else:
+# 								pressedOnce = False
+# 								firstTimePress = 0
+# 						else:
+# 							pressedOnce = True
+# 							firstTimePress = time.time()
+
+
+def getPreds(cam, client):
 	""" 
 	** Conn to relevant server
 	1. Find obstacle ahead
@@ -54,10 +57,6 @@ def getPreds(outputQueue, cam):
 	3. Detect objects
 	4. Return manual control whenever shit gets too real
 	"""
-	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	client.connect(("ec2-3-15-154-186.us-east-2.compute.amazonaws.com", 6666))
-	conn = client.makefile('wb')
-
 	logger.info("Camera started and server connected to!!!!")
 
 	try:
@@ -76,12 +75,14 @@ def getPreds(outputQueue, cam):
 
 				# Object detection first
 				objects = pickle.loads(client.recv(4096), encoding = "bytes")
-				# Lane tracking next
+				# Lane tracking
 				angle = client.recv(4096)
 				angle = float(angle.decode())
 
 				payload = {"detections": objects, "angle": angle}
-				outputQueue.put(payload)
+
+				# Decisions
+				yield payload
 				
 	except KeyboardInterrupt:
 		cam.close()
@@ -90,11 +91,10 @@ def getPreds(outputQueue, cam):
 	finally:
 		work = False
 		cam.close()
-		conn.close()
 		client.close()
 
 
-def makeDecisions(motor, servo, outputQueue):
+def makeDecisions(motor, servo, cam, client):
 	carCentre = 58
 	carLeft = 95
 	carRight = 15
@@ -108,69 +108,61 @@ def makeDecisions(motor, servo, outputQueue):
 
 	logger.info("Car starting on empty queue with default direction and speed")
 
-	status = outputQueue.empty()
-	while True: # Wait for queue
-		if not outputQueue.empty():
-			while True:
-				# motor.setDirection(direction)
-				# motor.throttle(amount)
-				logger.info("Car moving at {} direction and {} speed with queue".format(direction, amount))
+	drive = True
+	while True:
+		try:
+			logger.info("Car moving at {} direction and {} speed with queue".format(direction, amount))
+			payload = next(getPreds(cam, client))
+			objects = payload["detections"]
+			angle = payload["angle"]
 
-				payload = outputQueue.get()
-				objects = payload["detections"]
-				angle = payload["angle"]
-
-				classes = objects[b"classIds"]
-				lefts = objects[b"lefts"]
-				isStopSign = any(cls == 2 for cls in classes)
-				isObstacle = any((lef >= 100 and lef <= 200) for lef in lefts)
-				is50Kph = any(cls == 4 for cls in classes)
-				is120Kph = any(cls == 3 for cls in classes)
+			classes = objects[b"classIds"]
+			lefts = objects[b"lefts"]
+			isStopSign = any(cls == 2 for cls in classes)
+			isObstacle = any((lef >= 100 and lef <= 200) for lef in lefts)
+			is50Kph = any(cls == 4 for cls in classes)
+			is120Kph = any(cls == 3 for cls in classes)
 
 
-				if(isObstacle or isStopSign): # Object in the middle or stop?
-					logger.info("Stopping because obstacle = {}  or stop = {}".format(isObstacle, isStopSign))
-					motor.stop()
-					amount = 0
-					direction = 0
+			if(isObstacle or isStopSign): # Object in the middle or stop?
+				logger.info("Stopping because obstacle = {}  or stop = {}".format(isObstacle, isStopSign))
+				motor.stop()
+				amount = 0
+				direction = 0
 
-				else:
-					motor.setDirection(-1)
-					motor.throttle(amount)
+			else:
+				motor.setDirection(-1)
+				motor.throttle(amount)
 
-				# Execute angle
-				value = np.interp(angle, [carRight, carCentre, carLeft], [-1, 0, 1])
-				servo.setAngle(value, 0)
+			# Execute angle
+			value = np.interp(angle, [carRight, carCentre, carLeft], [-1, 0, 1])
+			servo.setAngle(value, 0)
 
-				#Speed limit in periphery?
-				if(is50Kph):
-					amount = 0.5
-					direction = -1
-				elif(is120Kph):
-					amount = 1
-					direction = -1
+			#Speed limit in periphery?
+			if(is50Kph):
+				amount = 0.5
+				direction = -1
+			elif(is120Kph):
+				amount = 1
+				direction = -1
+		except 
 
 
 def main(motor, servo):
-	q = multiprocessing.Queue()
+	# capture continuous just doesn't work if not in main thread.
 	# Init a bunch of things
 	cam = PiCamera()
 	cam.resolution = (640, 480)
 	cam.framerate = 15
 	time.sleep(2)
 
-	# Run as multiple processes
-	proc1 = multiprocessing.Process(target = getPreds, args = (q, cam, ))
-	proc2 = multiprocessing.Process(target = makeDecisions, args = (motor, servo, q, ))
-	proc3 = multiprocessing.Process(target = godMode)
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client.connect(("ec2-3-15-154-186.us-east-2.compute.amazonaws.com", 6666))
+	conn = client.makefile('wb')
 
-	proc1.start()
-	proc2.start()
-	proc3.start()
+	makeDecisions(motor, servo, cam, client)
+	
 
-	proc1.join()
-	proc2.join()
-	proc3.join()
 
 
 	
